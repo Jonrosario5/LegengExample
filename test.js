ab3468ff715fe4e965b507754bdea60c16923b6f
Jon Rosario <41496659+Jonrosario5@users.noreply.github.com>
2020-06-22 13:10:50 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 let jon = {
     testing:function(){
-        setTimeout(function() {
+        
            let chart = Highcharts.charts[0]
            chart.update({
                series:[{
                    color:"red"
                }]
            })
-            },100);
+
     }
 }
